2c208a3d900fbd3ad374ed6ac027aad5b63199a6
Mohammad Sayem <msayem089@gmail.com>
2021-04-03 12:43:07 +0600

    page 6 completed and issues fixed

--- scripts/app.js
+++
@@ -1,8 +0,0 @@
-//sidebar
-const menu = document.querySelector('.menu')
-const sidebar = document.querySelector('.sidebar div')
-menu.addEventListener('click', function () {
-    sidebar.classList.toggle('active')
-    menu.classList.toggle('fa-times')
-    menu.classList.toggle('fa-bars')
-})
