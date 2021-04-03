323b51159d2e8d0d945a4f22b72223fb17561ba8
Mohammad Sayem <msayem089@gmail.com>
2021-04-03 19:34:07 +0600

    page-8 completed

--- scripts/app.js
+++ scripts/app.js
@@ -2,8 +2,16 @@
 const chatMenu = document.querySelector('.chat-menu')
 const chatOpts = document.querySelector('.chat-opts')
 
-chatMenu.addEventListener('click', () => {
+chatMenu && chatMenu.addEventListener('click', () => {
     chatOpts.classList.toggle('active')
     chatMenu.classList.toggle('fa-times')
     chatMenu.classList.toggle('fa-ellipsis-v')
+})
+
+//mes menu
+const mesMenus = document.querySelectorAll('.mes-menu')
+mesMenus.forEach(mesMenu => {
+    mesMenu.addEventListener('click', function () {
+        this.nextElementSibling.classList.toggle('active')
+    })
 })
